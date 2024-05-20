e2dd4c2a6b7c02275ae044884e6cef8e96cbe797
abhishek-gangwar <abhishek-gangwar>
2024-05-21 00:48:51 +0530

    fix: speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 // libs
 import { RouterProvider } from "react-router-dom";
 
-// vercel speed insights
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
 // styles
 import "./App.css";
 
