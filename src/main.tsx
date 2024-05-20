f65d5d097e82e0ad5d53abe628e76d6286ba6432
abhishek-gangwar <abhishek-gangwar>
2024-05-21 00:52:55 +0530

    feat: revert speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// vercel speed insights
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>
 );
